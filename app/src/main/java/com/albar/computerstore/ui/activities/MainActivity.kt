411a6d5725922d8e95ea1e44af0c658bf1054157
albarfikri <fikrialbar4@gmail.com>
2022-04-09 15:54:00 +0700

    fixing bottom nav animation while clicking

--- app/src/main/java/com/albar/computerstore/ui/activities/MainActivity.kt
+++ app/src/main/java/com/albar/computerstore/ui/activities/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.albar.computerstore.ui.activities
 
 import android.annotation.SuppressLint
+import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.albar.computerstore.R
@@ -17,6 +19,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var _navHost: View
     private val navHost get() = _navHost
 
+    override fun onStart() {
+        removeEffectsWhileClickingFab(false)
+        super.onStart()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -24,29 +31,26 @@ class MainActivity : AppCompatActivity() {
 
         _navHost = findViewById(R.id.navHostFragment)
 
-        // botom navigation
+        // bottom navigation
         binding.bottomNavigationView.background = null
+        navigation()
 
         binding.login.setOnClickListener {
             Toast.makeText(this, "Hi There", Toast.LENGTH_SHORT).show()
         }
-
-        navigation()
-
     }
 
     private fun navigation() {
+        binding.fabLocation.setOnClickListener {
+            navHost.findNavController().navigate(R.id.action_global_locationFragment)
+            removeEffectsWhileClickingFab(false)
+        }
 
         binding.apply {
-            fabLocation.setOnClickListener {
-                navHost.findNavController().navigate(R.id.action_global_locationFragment)
-                removeAnimationWhileClickingFab(false)
-            }
-
-            removeAnimationWhileClickingFab(true)
             bottomNavigationView.setupWithNavController(navHost.findNavController())
             navHost.findNavController()
                 .addOnDestinationChangedListener { _, destination, _ ->
+                    removeEffectsWhileClickingFab(true)
                     when (destination.id) {
                         R.id.list, R.id.locationFragment, R.id.nearest ->
                             bottomNavigationView.visibility = View.VISIBLE
@@ -56,10 +60,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     @SuppressLint("ResourceAsColor")
-    private fun removeAnimationWhileClickingFab(status: Boolean) {
+    private fun removeEffectsWhileClickingFab(status: Boolean) {
         binding.bottomNavigationView.menu.getItem(0).isChecked = status
         binding.bottomNavigationView.menu.getItem(1).isChecked = status
+
+        if (!status) {
+            binding.fabLocation.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(this, R.color.colorPrimary))
+        } else {
+            binding.fabLocation.backgroundTintList =
+                ColorStateList.valueOf(ContextCompat.getColor(this, R.color.defaultColor))
+        }
     }
 }
