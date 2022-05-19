6b563f84609352c7b10d3fa887a61d85b431c259
albarfikri <fikrialbar4@gmail.com>
2022-05-19 20:29:54 +0700

    inflate Sign In and Sign Up Fragment into mainActivity

--- app/src/main/java/com/albar/computerstore/ui/activities/SignInSignUpActivity.kt
+++ app/src/main/java/com/albar/computerstore/ui/activities/SignInSignUpActivity.kt
@@ -3,6 +3,7 @@ package com.albar.computerstore.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.albar.computerstore.R
 import com.albar.computerstore.databinding.ActivitySignInSignUpBinding
 
@@ -10,16 +11,22 @@ class SignInSignUpActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInSignUpBinding
 
+    private lateinit var _navHost: NavHostFragment
+    private val navHost get() = _navHost
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInSignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        _navHost = supportFragmentManager.findFragmentById(R.id.navHostFragment2) as NavHostFragment
     }
 
     override fun onBackPressed() {
         val moveBackToMainActivity =
             Intent(this@SignInSignUpActivity, MainActivity::class.java)
-        startActivity(moveBackToMainActivity).apply{
+        startActivity(moveBackToMainActivity).apply {
             overridePendingTransition(R.anim.enter_from_left, R.anim.exit_to_right)
         }
         super.onBackPressed()
--- app/src/main/java/com/albar/computerstore/ui/fragments/login/SigninFragment.kt
+++ app/src/main/java/com/albar/computerstore/ui/fragments/login/SigninFragment.kt
@@ -7,26 +7,26 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.albar.computerstore.R
-import com.albar.computerstore.databinding.FragmentSignupBinding
+import com.albar.computerstore.databinding.FragmentSigninBinding
 
-class SignupFragment : Fragment() {
+class SigninFragment : Fragment() {
 
-    private var _binding: FragmentSignupBinding? = null
+    private var _binding: FragmentSigninBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+    ): View {
+        _binding = FragmentSigninBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.signIn.setOnClickListener {
-            findNavController().navigate(R.id.action_signupFragment_to_signinFragment)
+        binding.signUp.setOnClickListener {
+            findNavController().navigate(R.id.action_signinFragment_to_signupFragment)
         }
     }
 
@@ -34,4 +34,5 @@ class SignupFragment : Fragment() {
         super.onDestroy()
         _binding = null
     }
+
 }
